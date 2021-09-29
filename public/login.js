29b5e2c8bbef9d078362c47297d618e9ce1a8b0f
RickardA <rickard.andersson98@gmail.com>
2021-09-29 18:40:01 +0200

    cleaned up login and added so we sign in to backend

--- public/login.js
+++ public/login.js
@@ -1,5 +1,6 @@
 function handleCredentialResponse (response) {
       console.log("Encoded JWT ID token: " + response.credential);
+      loginAgainstBackend(response.credential)
 }
 window.onload = function () {
       google.accounts.id.initialize({
@@ -13,26 +14,6 @@ window.onload = function () {
       google.accounts.id.prompt(); // also display the One Tap dialog
 }
 
-onSignIn = (googleUser) => {
-      const profile = googleUser.getBasicProfile();
-      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-      console.log('Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-
-      console.log('ID_TOKEN: ', googleUser.getAuthResponse().id_token)
-      loginAgainstBackend(googleUser.getAuthResponse().id_token)
-}
-
-const signOut = () => {
-      const auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(function () {
-            console.log('User signed out.');
-      });
-}
-
-// Get current JWT console.log(gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().id_token)
-
 const loginAgainstBackend = (id_token) => {
       fetch('http://localhost:3000/auth/login', {
             method: 'POST',
@@ -42,5 +23,3 @@ const loginAgainstBackend = (id_token) => {
             body: JSON.stringify({ id_token })
       })
 }
-
-// 385996968425-0asav67051ro7oe509l603jvm7p88t1s.apps.googleusercontent.com
