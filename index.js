033e93121f51106606cf7634e6f5ddce6568cb89
RickardA <rickard.andersson98@gmail.com>
2021-09-29 13:35:09 +0200

    added dotenv, added mongoose, connect to db

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express')
+const helmet = require("helmet");
+const mongoose = require('mongoose');
+require('dotenv').config()
+
 const app = express()
- 
+
+app.use(helmet())
 app.use(express.static('public'))
 
 app.post('/login', function (req, res) {
-  res.send('Hello World ', req.body)
+      res.send('Hello World ', req.body)
 })
- 
+
 app.listen(3000, (error) => {
-      if(error) {
+      if (error) {
             console.error('Could not serve: ', error)
       }
       console.log('Listening on port 3000')
 })
+
+
+const connectToDb = async () => {
+      try {
+            console.log('Connecting to db... ')
+            await mongoose.connect(`mongodb+srv://rickard:${process.env.DB_PASSWORD}@cluster0.pn9qj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+            console.log('DB connected')
+      } catch (error) {
+            console.error('Could not connect to db: ', error)
+      }
+}
+
+connectToDb()
